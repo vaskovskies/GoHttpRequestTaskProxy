80b1f6fb6e1e548c2c6f23bfb06334928ac950fb
vaskovskies <belsaveliyvaskovskiy@gmail.com>
2025-06-19 16:32:38 +0300

    remove unneeded testing scripts (just use unit tests) also removed unneeded version comment from docker-compose.yml

--- scripts/testdelete.ps1
+++
@@ -1,8 +0,0 @@
-# Define the URL of the endpoint
-$url = "http://admin:secret@localhost:8080/task"
-
-# Send the GET request using Invoke-RestMethod
-$response = Invoke-RestMethod -Uri $url -Method Delete
-
-# Output the response
-$response | ConvertTo-Json -Depth 10
