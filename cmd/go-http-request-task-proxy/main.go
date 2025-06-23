84d2e7fd7ca5463ef1c1fac7906a0ba6973dbe3d
vaskovskies <belsaveliyvaskovskiy@gmail.com>
2025-06-23 19:15:11 +0300

    fix timeout to 5 seconds

--- cmd/go-http-request-task-proxy/main.go
+++ cmd/go-http-request-task-proxy/main.go
@@ -335,7 +335,7 @@ func main() {
 		}
 	}()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 0*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	quit := make(chan os.Signal, 1)
